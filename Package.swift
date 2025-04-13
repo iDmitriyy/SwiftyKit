9af2705f2538cbe67ea18523abc52f985cf3494d
Dmitriy Ignatyev <no_email_yet@git.ru>
2025-04-13 23:16:22 +0300

    init

--- Package.swift
+++ Package.swift
@@ -4,21 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyKit",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SwiftyKit",
-            targets: ["SwiftyKit"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftyKit"),
-        .testTarget(
-            name: "SwiftyKitTests",
-            dependencies: ["SwiftyKit"]
-        ),
-    ]
+  name: "swifty-kit",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(name: "SwiftyKit", targets: ["SwiftyKit"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.4")),
+    .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "1.2.0")),
+    .package(url: "https://github.com/pointfreeco/swift-nonempty.git", .upToNextMajor(from: "0.5.0")),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(name: "SwiftyKit", dependencies: [.target(name: "IndependentDeclarations"),
+                                              .target(name: "StdLibExtensions"),
+                                              .target(name: "FoundationExtensions"),
+                                              .product(name: "Collections", package: "swift-collections"),
+                                              .product(name: "Algorithms", package: "swift-algorithms"),
+                                              .product(name: "NonEmpty", package: "swift-nonempty")]),
+    .target(name: "StdLibExtensions"),
+    .target(name: "FoundationExtensions"),
+    
+    // MARK: - Test Targets
+    
+    .testTarget(name: "SwiftyKitTests", dependencies: ["SwiftyKit"]),
+  ]
 )
