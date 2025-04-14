b65d4ec12e6c8ae08c52ec8b0e7feed1d2ab6ba2
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-04-14 18:20:45 +0300

    minor

--- Sources/FoundationExtensions/_ReExport.swift
+++ Sources/FoundationExtensions/_ReExport.swift
@@ -9,8 +9,8 @@
 @_exported public import struct Foundation.CharacterSet
 @_exported public import protocol Foundation.LocalizedError
 
-@_exported import IndependentDeclarations
-
-extension TextError: LocalizedError {
-  @usableFromInline package var errorDescription: String? { text }
-}
+//@_exported import IndependentDeclarations
+//
+//extension TextError: LocalizedError { // lead to compiler error in another package: Cannot find type 'TextError' in scope
+//  @usableFromInline package var errorDescription: String? { text }
+//}
