e5ff77bf9ff68e2ae23cbc34ae9aa1937ae8d4d2
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-04-16 19:50:48 +0300

    in progress

--- Sources/SwiftyKit/ErrorInfo/ErrorInfoKeysTransform.swift
+++ Sources/SwiftyKit/ErrorInfo/ErrorInfoKeysTransform.swift
@@ -45,45 +45,105 @@ extension DictionaryUnifyingRootProtocol {}
 
 extension ErronInfoKey {
   internal static func fromAnyStyleToCamelCased(string: String) -> String {
+//    var result = ""
+//    var hadNonSepratorBefore = false
+//    var previousWasSeprator = false
+//    for (intIndex, character) in string.enumerated() {
+//      if character == "_" {
+//        previousWasSeprator = true
+//      } else if character == "-" {
+//        previousWasSeprator = true
+//      } else {
+//        // TODO: If no transform was made, then self can be returned without making a copy to result.
+//        // save a flag to remeber if any trnasforms were made, save only current index and make actual transform
+//        // and result allocation only when trasform really needed.
+//        if character.isUppercase {
+//          if intIndex == 0 {
+//            result.append(character.lowercased()) // lowercase first letter if kebbab
+//          } else {
+//            result.append(character) // all uppercased letters (except first letter in kebab style) copied as is
+//          }
+//        } else {
+//          if previousWasSeprator, hadNonSepratorBefore {
+//            result.append(character.uppercased())
+//          } else {
+//            result.append(character)
+//          }
+//        }
+//        hadNonSepratorBefore = true
+//        previousWasSeprator = false
+//      } // end if
+//    } // end for
+//    return hadNonSepratorBefore ? result : string
+    toPascalOrCamel(string: string, firstCharTransform: { "\($0)" })
+  }
+  
+  internal static func fromAnyStyleToPascalCased(string: String) -> String {
+    toPascalOrCamel(string: string, firstCharTransform: { $0.uppercased() })
+  }
+  
+  private static func toPascalOrCamel(string: String, firstCharTransform: (Character) -> String) -> String {
     var result = ""
-    var hadNonSepratorBefore = false
+    var normalCharsCount: UInt = 0
+    var hadNormalCharsBefore: Bool { normalCharsCount != 0}
     var previousWasSeprator = false
-    for (intIndex, character) in string.enumerated() {
+    for character in string {
       if character == "_" {
         previousWasSeprator = true
       } else if character == "-" {
         previousWasSeprator = true
-      } else {
+      } else { // non-separator character
         // TODO: If no transform was made, then self can be returned without making a copy to result.
         // save a flag to remeber if any trnasforms were made, save only current index and make actual transform
         // and result allocation only when trasform really needed.
-        if character.isUppercase {
-          if intIndex == 0 {
-            result.append(character.lowercased()) // lowercase first letter if kebbab
-          } else {
-            result.append(character) // all uppercased letters (except first letter in kebab style) copied as is
-          }
-        } else {
-          if previousWasSeprator, hadNonSepratorBefore {
+        // Если после сепаратора то uppercased
+        if previousWasSeprator {
+          if hadNormalCharsBefore {
             result.append(character.uppercased())
           } else {
+            result.append(firstCharTransform(character))
+          }
+        } else { // -h
+          if normalCharsCount > 0 {
             result.append(character)
+          } else {
+            result.append(firstCharTransform(character))
           }
         }
-        hadNonSepratorBefore = true
+        
+//        if character.isUppercase {
+//          if nonSeparatorCount == 0 {
+//            // lowercase first letter for camel
+//            // uppercase first letter for kebbab
+//            result.append(firstCharTransform(character))
+//          } else {
+//            result.append(character) // all uppercased letters (except first letter) copied as is
+//          }
+//        } else { // "-h"
+//          if previousWasSeprator, hadNormalCharsBefore { // camel
+//            result.append(character.uppercased())
+//          } else {
+//            result.append(character)
+//          }
+//          
+//          if previousWasSeprator { // pascal
+//            result.append(character.uppercased())
+//          } else {
+//            result.append(character)
+//          }
+//        }
+        normalCharsCount += 1
         previousWasSeprator = false
       } // end if
     } // end for
-    return result
+    return hadNormalCharsBefore ? result : string
   }
   
   internal static func snakeCased(string: String) -> String {
     fatalError()
   }
   
-  internal static func pascalCased(string: String) -> String {
-    fatalError()
-  }
+  
   
   internal static func kebabCased(string: String) -> String {
     fatalError()
--- Tests/SwiftyKitTests/ErrorInfo/ErrorInfoKeysTransformTests.swift
+++ Tests/SwiftyKitTests/ErrorInfo/ErrorInfoKeysTransformTests.swift
@@ -10,11 +10,16 @@ import Foundation
 import Testing
 
 struct ErrorInfoKeysTransformTests {
-  private static let expectedToBeUnmodified: [String] = [
-    "string.with.dots",
+  private static let expectedToStayUnmodified: [String] = [
     "",
     " ",
     "  ",
+  ]
+  
+  private static let expectedModifiedForPascalOnly: [String] = [ // for pascal first char will be uppercased
+    "string.with.dots",
+    "string.WiTh.dOtS2",
+    "string&WIth*|",
   ]
   
   private let keys: [String] = [
@@ -22,10 +27,15 @@ struct ErrorInfoKeysTransformTests {
     "camelCaseKey",
     "kebab-case-key",
     "PascalCaseKey",
+    "this-is-a-test-key",
+    "This-Is-Mixed-CASE",
+    "underscore_and-hyphen",
     "____many___underscores__",
     "----many---hyphens--",
-    "RepeatedUPPERCASE"
-  ] + expectedToBeUnmodified
+    "_________", // only underscores
+    "---------", // only hyphens
+    "RepeatedUPPERCASE",
+  ] + expectedToStayUnmodified + expectedModifiedForPascalOnly
   // TODO: Add not only english capitalized
   
   @Test func fromAnyStyleToCamelCased() throws {
@@ -36,11 +46,39 @@ struct ErrorInfoKeysTransformTests {
       "camelCaseKey",
       "kebabCaseKey",
       "pascalCaseKey",
+      "thisIsATestKey",
+      "thisIsMixedCASE",
+      "underscoreAndHyphen",
       "manyUnderscores",
       "manyHyphens",
-      "repeatedUPPERCASE"
-    ] + Self.expectedToBeUnmodified
+      "_________", // only underscores
+      "---------", // only hyphens
+      "repeatedUPPERCASE",
+    ] + Self.expectedToStayUnmodified + Self.expectedModifiedForPascalOnly
     
-    #expect(camelCased == expected)
+    let diff = Set(camelCased).symmetricDifference(expected)
+    #expect(diff.isEmpty, "\(diff)")
+  }
+  
+  @Test func fromAnyStyleToPascalCased() throws {
+    let pascalCased = keys.map(ErronInfoKey.fromAnyStyleToPascalCased(string:))
+    
+    let expected = [
+      "SnakeCaseKey",
+      "CamelCaseKey",
+      "KebabCaseKey",
+      "PascalCaseKey",
+      "ThisIsATestKey",
+      "ThisIsMixedCASE",
+      "UnderscoreAndHyphen",
+      "ManyUnderscores",
+      "ManyHyphens",
+      "_________", // only underscores
+      "---------", // only hyphens
+      "RepeatedUPPERCASE",
+    ] + Self.expectedToStayUnmodified + Self.expectedModifiedForPascalOnly.map { $0.uppercasingFirstLetter() }
+    
+    let diff = Set(pascalCased).symmetricDifference(expected)
+    #expect(diff.isEmpty, "\(diff)")
   }
 }
