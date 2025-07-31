301ba4d8f49dd56c3d79c3bea56ff6c2bfadab58
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-07-31 14:18:46 +0300

    tests

--- Tests/MacrosTests/UpdatableCopyMacroTests.swift
+++ Tests/MacrosTests/UpdatableCopyMacroTests.swift
@@ -5,23 +5,54 @@
 //  Created by Dmitriy Ignatyev on 30/07/2025.
 //
 
+//import SwiftSyntaxMacrosTestSupport
+
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import SwiftSyntaxMacrosGenericTestSupport
+import SwiftSyntax
+import SwiftDiagnostics
+
 #if canImport(MacroImps)
 import MacroImps
-import Macros
+//import Macros
 import XCTest
 
 final class UpdatableCopyMacroTests: XCTestCase {
-  private let macros = ["URL": UpdatableCopyMacro.self]
+  private let macros = ["UpdatableCopyMacro": UpdatableCopyMacro.self]
   
+  func testExpansionWithValidStruct() {
+//    print("_____________ UpdatableCopyMacro")
+    let input = """
+        struct Product {
+          let name: String
+          let price: Double
+          let oldPrice: Double?
+        }
+      """
+    
+    let expected = """
+        struct Product {
+          let name: String
+          let price: Double
+          let oldPrice: Double?
+        }
+      """
+    // func copyUpdating()
+    assertMacroExpansion(input,
+                         expandedSource: expected,
+                         macros: macros,
+                         indentationWidth: .spaces(2))
+  }
 }
 
 extension UpdatableCopyMacroTests {
-  @UpdatableCopy
-  struct Product {
-    let name: String
-    let price: Double
-    let oldPrice: Double?
-  }
+//  @UpdatableCopy
+//  struct Product {
+//    let name: String
+//    let price: Double
+//    let oldPrice: Double?
+//  }
 }
 
 #endif
