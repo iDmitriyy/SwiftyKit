e7dc4f393a077b05b67ddc3637f5bd330e9e5d2e
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-08-01 19:03:12 +0300

    ErrorInfoSubscriptTests

--- Sources/ErrorInfo/ErrorInfoProtocols.swift
+++ Sources/ErrorInfo/ErrorInfoProtocols.swift
@@ -33,6 +33,9 @@ public protocol ErrorInfoCollection: Collection, Sendable, CustomStringConvertib
 //  func asLegacyDictionary() -> [String: Any]
 }
 
+/// If a collision happens, then two symbols are used as a start of random key suffix:
+/// for subscript: `$` , e.g. "keyName$Ta5"
+/// for merge functions: `#` , e.g. "keyName_don0_file_line_FileName_81_#Wng"
 public protocol ErrorInfoType: ErrorInfoCollection {
   
 }
--- Tests/ErrorInfoTests/ErrorInfoSubscriptTests.swift
+++ Tests/ErrorInfoTests/ErrorInfoSubscriptTests.swift
@@ -9,15 +9,31 @@
 import Testing
 
 struct ErrorInfoSubscriptTests {
-  @Test func subscriptWithCollisions() async throws {
+  @Test func subscriptKeyCollisionsForEqualValues() async throws {
+    var errorInfo = ErrorInfo()
+    let key = "key"
+    errorInfo[key] = 0
+    errorInfo[key] = 0
+    errorInfo[key] = 0
+    
+    #expect(errorInfo.asStringDict().keys.count == 1)
+  }
+  
+  @Test func subscriptKeyCollisionsForNotEqualValues() async throws {
     var errorInfo = ErrorInfo()
     let key = "key"
     errorInfo[key] = 0
     errorInfo[key] = 1
     errorInfo[key] = 2
     
-    let keys = errorInfo.asStringDict().keys
-    
+    let keys = errorInfo.asStringDict().keys.sorted(by: { $0.count < $1.count })
     #expect(keys.count == 3)
+    #expect(keys[0].count == 3)
+    #expect(keys[1].count == 7)
+    #expect(keys[2].count == 7) // FIXME: flaky test, if second key2 will collide with key1, then key2 will have cunt = 10
+    
+    // 0 : "key"
+    // 1 : "key$lia"
+    // 2 : "key$Qet"
   }
 }
