f4ba4d1b3b8e5794545ea671bfa3bc47b1c88974
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-09-18 02:09:24 +0300

    add comment

--- Sources/ErrorInfo/ErrorInfo+DictionaryLiteral.swift
+++ Sources/ErrorInfo/ErrorInfo+DictionaryLiteral.swift
@@ -10,6 +10,9 @@
 extension ErrorInfo: ExpressibleByDictionaryLiteral {
   public typealias Value = (any ErrorInfoValueType)?
   public typealias Key = String
+  // FIXME: can optional ErrorInfoValueType be used without conflict with ErrorInfoIterable protocol
+  // Alternative: if optionals are impossible for DictionaryLiteral usage, then add functionBuilder initialization that allows
+  // optional values
   
   public init(dictionaryLiteral elements: (String, Value)...) {
     self.init()
