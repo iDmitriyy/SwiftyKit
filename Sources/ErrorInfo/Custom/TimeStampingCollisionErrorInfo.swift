e17f6702a75043823e4399fabb7d641b708d63f6
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-09-18 02:52:53 +0300

    Create TimeStampingCollisionErrorInfo.swift

---
+++ Sources/ErrorInfo/Custom/TimeStampingCollisionErrorInfo.swift
@@ -0,0 +1,12 @@
+//
+//  TimeStampingErrorInfo.swift
+//  swifty-kit
+//
+//  Created by Dmitriy Ignatyev on 18/09/2025.
+//
+
+struct TimeStampingCollisionErrorInfo {
+  private var storage: MultiValueErrorInfoGeneric<String, any ErrorInfoValueType>
+  
+  // ContinuousClock.Instant
+}
