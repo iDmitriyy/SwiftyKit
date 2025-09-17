3ac3d02024973b0fc9536e30957d0ce411769d15
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-09-18 02:33:52 +0300

    in prgress

--- Sources/ErrorInfo/Protocols/KeyAugmentationStrategy/KeyAugmentationStrategy+AsDictionary.swift
+++ Sources/ErrorInfo/Protocols/KeyAugmentationStrategy/KeyAugmentationStrategy+AsDictionary.swift
@@ -9,13 +9,18 @@ extension ErrorInfoUniqueKeysAugmentationStrategy where Self: ErrorInfoPartialCo
   public func asGenericDict<D>() -> D where D: DictionaryUnifyingProtocol<Key, Value> {
     var recipient = D(minimumCapacity: self.count)
     
-    for (key, value) in self.keyValuesView {
-      // TODO: crashing the app seems not be a good solution.
+    let keyVaues = self.keyValuesView
+    var keyVauesCount: Int = 0
+    for (key, value) in keyVaues {
+      // TODO: crashing the app by precondition seems not be a good solution.
       // can it be statically proven? If not, how logging can be added?
       // finally, check that crash (not)happens if invariant is (not)violated
-      precondition(!recipient.hasValue(forKey: key), "")
+      
       recipient[key] = value
+      keyVauesCount += 1
     }
+    // keyVaues must contain unique keys (when get from ErrorInfo with UniqueKeysAugmentationStrategy)
+    precondition(recipient.count == keyVauesCount)
     
     return recipient
   }
